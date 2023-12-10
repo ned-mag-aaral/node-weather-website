7debd15a93ff6175dab5fc86931cbaed59f746d8
Cayetano <933617@cognizant.com>
2023-12-10 16:22:42 +0800

    Set up for deployment

--- public/js/app.js
+++ public/js/app.js
@@ -9,11 +9,10 @@ weatherForm.addEventListener('submit', (event) => {
     event.preventDefault()
     
     const location = search.value
-    const url = 'http://localhost:3000/weather?address=' + location
 
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
-    fetch(url).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 messageOne.textContent = data.error
